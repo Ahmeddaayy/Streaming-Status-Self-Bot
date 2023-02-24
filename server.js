3382b78baae304711cc4119c73c63b82b48a9843
Malik Ahmed <102971549+MaybeAhmed@users.noreply.github.com>
2023-02-24 04:58:14 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require("express")
 const server = express()
 
 server.all("/", (req, res) => {
-  res.send('Connect Me To A 24/7 Hosting || Made By RootPlayZ#0001')
+  res.send('Connect Me To A 24/7 Hosting || Made By Ahmed.#0001')
 })
 
 function keepAlive() {
   server.listen(3000, () => {
-    console.log("Creator:RootPlayZ#0001")
+    console.log("Creator: Ahmed.#0001")
   })
   
 }
